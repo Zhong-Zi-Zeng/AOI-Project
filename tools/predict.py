78aa8630c015a7c9a4e1dc88f61d03407fdb0391
Zhong-Zi-Zeng <ximen0806449@gmail.com>
2023-12-28 13:24:08 +0800

    新增predict.py，可以直接顯示結果並儲存影像

---
+++ tools/predict.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+import sys
+import os
+
+sys.path.append(os.path.join(os.getcwd()))
+from engine.builder import Builder
+from engine.general import get_work_dir_path
+from tqdm import tqdm
+from pathlib import Path
+import argparse
+import cv2
+import os
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('Model training script.', add_help=False)
+
+    parser.add_argument('--config', '-c', type=str, required=True,
+                        help='The path of config.')
+
+    parser.add_argument('--source', '-s', type=str, required=True,
+                        help='An image path or a directory path')
+
+    parser.add_argument('--show', action='store_true',
+                        help='Whether show the resulting image.')
+
+    return parser
+
+def run():
+    if os.path.isdir(args.source):
+        file_list = os.listdir(args.source)
+        source = [os.path.join(args.source, item) for item in file_list]
+    elif os.path.isfile(args.source):
+        source = [args.source]
+    else:
+        ValueError("Cannot find the source file {}".format(args.source))
+
+    for image_file in tqdm(source, total=len(source)):
+        image = cv2.imread(image_file)
+        result = model.predict(image,
+                               conf_thres=cfg["conf_thres"],
+                               nms_thres=cfg["nms_thres"])
+
+        result_image = result['result_image']
+
+        if args.show:
+            result_image_cp = result_image.copy()
+            result_image_cp = cv2.resize(result_image_cp, None, fx=0.3, fy=0.3)
+            cv2.imshow(f"{image_file}", result_image_cp)
+            cv2.waitKey(0)
+
+        cv2.imwrite(os.path.join(get_work_dir_path(cfg), Path(image_file).name), result_image)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('Model evaluation script.',
+                                     parents=[get_args_parser()])
+    args = parser.parse_args()
+
+    # Create builder
+    builder = Builder(config_path=args.config, task='predict')
+
+    # Build config
+    cfg = builder.build_config()
+
+    # Build model
+    model = builder.build_model(cfg)
+
+    run()
