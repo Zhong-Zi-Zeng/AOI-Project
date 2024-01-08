d5074f8c11eb0d7d4c1f3a643acc70cab231cf20
Zhong-Zi-Zeng <ximen0806449@gmail.com>
2024-01-08 21:17:14 +0800

    1. cascade_mask_rcnn.py、mask2former 原本在predict時沒有過濾掉score太低和設定nms threshold (已修復) 2. general.py 原本無法更新float (已修復)

--- model_zoo/mmdetection/models/cascade_mask_rcnn.py
+++ model_zoo/mmdetection/models/cascade_mask_rcnn.py
@@ -10,6 +10,7 @@ from engine.timer import TIMER
 from mmdet.apis import DetInferencer
 import numpy as np
 import subprocess
+import cv2
 
 
 class CascadeMaskRCNN(BaseInstanceModel):
@@ -52,7 +53,8 @@ class CascadeMaskRCNN(BaseInstanceModel):
             'warmup_begin': self.cfg['start_epoch'],
             'warmup_end': self.cfg['warmup_epoch'],
             'optim_wrapper': optimizer,
-            'default_hooks': dict(checkpoint=dict(type='CheckpointHook', interval=self.cfg['save_period']))
+            'check_interval': self.cfg['save_period'],
+            'nms_threshold': self.cfg['nms_thres'],
         }
 
         update_python_file(self.cfg['cfg_file'], os.path.join(get_work_dir_path(self.cfg), 'cfg.py'), variables)
@@ -80,27 +82,46 @@ class CascadeMaskRCNN(BaseInstanceModel):
         if not hasattr(self, 'model'):
             self._load_model()
 
-
         with TIMER[0]:
-            result = self.model(source, show=False, print_result=False, return_vis=True)
+            with TIMER[1]:
+                # Load image
+                if isinstance(source, str):
+                    original_image = cv2.imread(source)
+                elif isinstance(source, np.ndarray):
+                    original_image = source
+                else:
+                    raise ValueError
+
+            result = self.model(original_image, show=False, print_result=False, return_vis=True)
+
+            class_list = []
+            score_list = []
+            bbox_list = []
+            rle_list = []
 
             predictions = result['predictions'][0]
             vis = result['visualization'][0]
-            labels = predictions['labels']
+            classes = predictions['labels']
             scores = predictions['scores']
-            rle_list = predictions['masks']
+            rles = predictions['masks']
+            bboxes = predictions['bboxes']
 
-            # TODO: 過濾score小於conf_thres的預測
-            _bboxes = []
-            for bbox in predictions['bboxes']:
+            for cls, conf, bbox, rle in zip(classes, scores, bboxes, rles):
+                if conf < conf_thres:
+                    continue
+
                 x = bbox[0]
                 y = bbox[1]
                 w = bbox[2] - x
                 h = bbox[3] - y
-                _bboxes.append(list(map(float, [x, y, w, h])))
+
+                class_list.append(cls)
+                score_list.append(conf)
+                bbox_list.append(list(map(float, [x, y, w, h])))
+                rle_list.append(rle)
 
         return {"result_image": vis,
-                "class_list": labels,
-                "bbox_list": _bboxes,
-                "score_list": scores,
+                "class_list": class_list,
+                "bbox_list": bbox_list,
+                "score_list": score_list,
                 "rle_list": rle_list}
