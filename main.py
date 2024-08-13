b122a7e9a4cd67d5c5c973ce089159ee1103e797
Zhong-Zi-Zeng <ximen0806449@gmail.com>
2024-08-13 16:15:35 +0800

    [Bug] 修復使用wsl執行時, main.py無法判斷要拉windows的image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
 import platform
-
+import os
 
 def get_gpu_count():
     try:
@@ -15,11 +15,22 @@ def get_gpu_count():
 
 def get_os_type():
     os_type = platform.system()
-    return os_type
+    if os_type == "Linux":
+        if os.path.isfile('/proc/sys/kernel/osrelease'):
+            with open('/proc/sys/kernel/osrelease') as f:
+                os_release = f.read().lower()
+                if 'microsoft' in os_release or 'wsl' in os_release:
+                    return "WSL"
+        return "Linux"
+    elif os_type == "Windows":
+        return "Windows"
+    else:
+        return os_type
 
 
 def generate_docker_compose(gpu_count, os_type):
-    image = "miislab/aoi_cuda11.8:windows" if os_type == "Windows" else "miislab/aoi_cuda11.8:ubuntu"
+    image = "miislab/aoi_cuda11.8:windows" if os_type == "Windows" or "WSL" \
+        else "miislab/aoi_cuda11.8:ubuntu"
     with open('docker-compose-template.yml', 'r') as file:
         compose_template = file.read()
 
@@ -42,6 +53,7 @@ def run_docker_compose():
 def main():
     gpu_count = get_gpu_count()
     os_type = get_os_type()
+    print(os_type)
     generate_docker_compose(gpu_count, os_type)
     run_docker_compose()
 
