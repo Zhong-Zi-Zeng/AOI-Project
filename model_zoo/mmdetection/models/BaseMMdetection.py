a7e4c948f77ee8c7e609d1a4c0fc263a623ad11e
Zhong-Zi-Zeng <ximen0806449@gmail.com>
2024-08-23 13:40:43 +0800

    [Bug] 如果使用resume training的話, 需要檢測end_epoch是否有大於weight裡的最後一個epoch

--- model_zoo/mmdetection/models/BaseMMdetection.py
+++ model_zoo/mmdetection/models/BaseMMdetection.py
@@ -7,7 +7,7 @@ import torch
 from mmdet.apis import DetInferencer
 
 from engine.general import (get_model_path, get_work_dir_path, load_python,
-                            update_python_file, get_device)
+                            update_python_file, get_device, check_path)
 
 
 class BaseMMdetection:
@@ -96,10 +96,19 @@ class BaseMMdetection:
         """
         work_dir_path = get_work_dir_path(self.cfg)
         checkpoint = torch.load(self.cfg["weight"])
+
         checkpoint['meta']['epoch'] = 0
         checkpoint['meta']['iter'] = 0
         torch.save(checkpoint, os.path.join(work_dir_path, 'pretrained_weight.pth'))
         self.cfg["weight"] = os.path.join(work_dir_path, 'pretrained_weight.pth')
+
+    def _check_valid_epoch(self):
+        """
+        Check if the end_epoch is valid.
+        """
+        checkpoint = torch.load(self.cfg["weight"])
+        assert self.cfg['end_epoch'] > checkpoint['meta']['epoch'], \
+            'The end_epoch must be larger than the checkpoint epoch'
 
     def update_config_var(self, var_dict: dict):
         assert isinstance(var_dict, dict)
@@ -122,9 +131,11 @@ class BaseMMdetection:
             command = f'{dist_train_sh} {self.cfg["cfg_file"]} 1 --work-dir {get_work_dir_path(self.cfg)}'
 
             if self.cfg['weight'] is not None:
-                assert os.path.exists(self.cfg['weight']), "The weight file does not exist."
+                assert check_path(self.cfg['weight']), "The weight file does not exist."
 
-                if not self.cfg['resume_training']:
+                if self.cfg['resume_training']:
+                    self._check_valid_epoch()
+                else:
                     self._reset_epoch_and_iter()
 
                 command += f' --resume {self.cfg["weight"]}'
