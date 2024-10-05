b4755b6d5cd7561996bf1ff8acc891baa10850f0
Zhong-Zi-Zeng <ximen0806449@gmail.com>
2024-10-05 18:18:34 +0800

    V1.0.4 [Bug] 修復如果是resume training的話, 在record loss時會發生錯誤的問題

--- model_zoo/mmdetection/mmdet/engine/hooks/record_training_loss.py
+++ model_zoo/mmdetection/mmdet/engine/hooks/record_training_loss.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from mmengine.hooks import Hook
 from mmdet.registry import HOOKS
 
-from engine.general import (save_json, TEMP_DIR, ROOT, load_json)
+from engine.general import (save_json, TEMP_DIR, ROOT, load_json, check_path)
 
 @HOOKS.register_module()
 class RecordTrainingLossHook(Hook):
@@ -23,18 +23,20 @@ class RecordTrainingLossHook(Hook):
         loss = outputs['loss'].item()
         iter = runner.iter
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        loss_json_path = os.path.join(ROOT, TEMP_DIR, 'loss.json')
         data = {'iter': iter, 'loss': loss, 'time': current_time}
 
         # If iter == 0, create loss.json
         # Else, append data to loss.json
-        if iter == 0:
-            save_json(os.path.join(ROOT, TEMP_DIR, 'loss.json'),
-                      [data])
-            return
+        if iter == 0 or \
+                not check_path(loss_json_path):
+            save_json(loss_json_path, [data])
 
         elif iter % 5 == 0:
-            history_data = load_json(os.path.join(ROOT, TEMP_DIR, 'loss.json'))
-            history_data.append(data)
-            save_json(os.path.join(ROOT, TEMP_DIR, 'loss.json'),
-                      history_data)
+            history_data = load_json(loss_json_path)
+            if isinstance(history_data, list):
+                history_data.append(data)
+                save_json(loss_json_path, history_data)
+            else:
+                save_json(loss_json_path, [data])
 
--- model_zoo/yolov7_inSeg/hooks/record_training_loss.py
+++ model_zoo/yolov7_inSeg/hooks/record_training_loss.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 sys.path.append(os.path.join(os.getcwd()))
 
-from engine.general import ROOT, TEMP_DIR, save_json, load_json
+from engine.general import (ROOT, TEMP_DIR, save_json, load_json, check_path)
 
 
 class RecordTrainingLossHook:
@@ -13,17 +13,19 @@ class RecordTrainingLossHook:
                loss: float):
 
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        loss_json_path = os.path.join(ROOT, TEMP_DIR, 'loss.json')
         data = {'iter': iter, 'loss': loss, 'time': current_time}
 
         # If iter == 0, create loss.json
         # Else, append data to loss.json
-        if iter == 0:
-            save_json(os.path.join(ROOT, TEMP_DIR, 'loss.json'),
-                      [data])
-            return
+        if iter == 0 or \
+                not check_path(loss_json_path):
+            save_json(loss_json_path, [data])
 
         elif iter % 5 == 0:
-            history_data = load_json(os.path.join(ROOT, TEMP_DIR, 'loss.json'))
-            history_data.append(data)
-            save_json(os.path.join(ROOT, TEMP_DIR, 'loss.json'),
-                      history_data)
+            history_data = load_json(loss_json_path)
+            if isinstance(history_data, list):
+                history_data.append(data)
+                save_json(loss_json_path, history_data)
+            else:
+                save_json(loss_json_path, [data])
